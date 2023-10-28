0b726ef2c9408ae856745b9bab34e1e5ca8b492b
Onyedikachi123 <onyedikachierugo@gmail.com>
2023-10-28 20:53:57 +0100

    header and hero section fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,92 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
 import Button from "../Button/Button";
-import { NavLink } from "react-router-dom";
-import { motion } from "framer-motion";
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => setSidebarOpen((prevState) => !prevState);
-
-  const sidebarVariants = {
-    open: { x: 0 },
-    closed: { x: "100%" },
-  };
-
   return (
     <div className="header container">
       <img src={Logo} alt="Logo" className="logo" />
-      <button className="hamburger" onClick={toggleSidebar}>
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M3 12H21M3 6H21H3ZM3 18H21H3Z"
-            stroke="#0197B2"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </button>
+      <ul className="header-menu">
+        <li><NavLink exact to="/"  activeClassName="active">Home</NavLink></li>
+        <li><NavLink to="/category"  activeClassName="active">Category</NavLink></li>
+        <li><NavLink to="/about"  activeClassName="active">About us</NavLink></li>
+        <li><NavLink to="contact"  activeClassName="active">Contact us</NavLink></li>
+      </ul>
 
-      <motion.div
-        className="menu-wrapper"
-        initial="closed"
-        animate={isSidebarOpen ? "open" : "closed"}
-        variants={sidebarVariants}
-      >
-        <ul className="header-menu">
-          <li>
-            <NavLink
-              exact
-              to="/"
-              activeClassName="active"
-              onClick={toggleSidebar}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/category"
-              activeClassName="active"
-              onClick={toggleSidebar}
-            >
-              Category
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about"
-              activeClassName="active"
-              onClick={toggleSidebar}
-            >
-              About us
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/contact"
-              activeClassName="active"
-              onClick={toggleSidebar}
-            >
-              Contact us
-            </NavLink>
-          </li>
-        </ul>
-
-        <div className="button-group">
-          <Button text="Log in" to="/login" variant="secondary" />
-          <Button text="Register" to="/register" variant="primary" />
-        </div>
-      </motion.div>
+      <div className="button-group">
+        <Button text="Log in" to="/login" variant="secondary" />
+        <Button text="Register" to="/register" variant="primary" />
+      </div>
     </div>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,25 +6,23 @@ import Businessmen from "../../assets/businessmen.png";
 
 const Hero = () => {
   return (
-    <div className="hero container-fluid">
-      <div className="background-image row">
-      <div className="col-12 text-center">
-          <h1>
-            Unlock the World of
-            <span> Currency Exchange</span> Connect, Trade, and Hagull.
-          </h1>
-          <p>
-            A marketplace that empowers individuals worldwide to effortlessly buy,
-            sell, and exchange currencies.
-          </p>
-          <button className="centered-button">
-            <Link to="/category">Explore Marketplace</Link>
-          </button>
-        </div>
+    <div className="hero">
+      <div className="background-image">
+        <h1>
+          Unlock the World of
+          <span> Currency Exchange</span> Connect, Trade, and Hagull.
+        </h1>
+        <p className="pt-3">
+          A marketplace that empowers individuals worldwide to effortlessly buy,
+          sell, and exchange currencies.
+        </p>
       </div>
       <div className="floating-image">
         <img src={Currency} alt="Currency" className="cuurency" />
       </div>
+      <button className="centered-button">
+        <Link to="/category">Explore Marketplace</Link>
+      </button>
       <div className="centered-image">
         <img src={Businessmen} alt="businessmen" />
       </div>
